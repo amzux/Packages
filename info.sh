82e5b5297e249a101bc8185cfafb31ede24c2247
Dr.Amzker <amzker@Amzux.Amzux>
2020-09-14 13:07:00 -0400

    Advance beauty

--- info.sh
+++ info.sh
@@ -5,5 +5,7 @@
 #rm -r list.sh.info README.md.info
 
 for we in $(ls *Amzx) ; do
+#echo "----------------------------------------------">>list.txt
 echo $we "=>" $(cat $we.info)>>list.txt
+echo "----------------------------------------------">>list.txt
 done
